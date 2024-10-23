da650c541ad57e374bd6289ce89ee186ae789609
rod-nickle <rod@bitwise-it.com>
2024-10-23 10:37:30 -0600

    Added timer1.js

--- timer1.js
+++ timer1.js
@@ -1,1 +1,17 @@
-//
+// This is an alarm clock / timer which will beep after a specified amount of time has passed.
+// The user can specify an unlimited number of alarms using command line arguments
+
+const beep = 'Beep!\n';
+const alarms = process.argv.slice(2);
+let seconds = 0;
+
+for (let alarm of alarms) {
+  seconds = Number(alarm);
+  if (isNaN(seconds) || seconds < 0) {
+    continue;
+  }
+
+  setTimeout(() => {
+    process.stdout.write(beep);
+  }, (seconds * 1000));
+}
